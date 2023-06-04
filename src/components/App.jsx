4f7c1daddcd6bf74600768c62eda212a7c84460a
xxxRomanxxx <112776664+xxxRomanxxx@users.noreply.github.com>
2023-06-04 22:19:52 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,15 @@ import { nanoid } from 'nanoid';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: '',
     name: '',
-    number: '',
+    number: ''
   }
 
   nameId = nanoid();
@@ -26,6 +32,7 @@ class App extends Component {
 
   addContact = () => {
     this.setState({...this.state.contacts.push(this.state.name)})
+    this.setState({...this.state.contacts.push(this.state.number)})
   }
 
 
